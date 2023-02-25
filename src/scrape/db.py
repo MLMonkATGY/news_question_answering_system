b734dd867e65346dd4b131a877f42db64ac5a95e
alextay96 <alextaygeeyang96@gmail.com>
2023-02-25 13:38:59 +0800

    Database setup Fixes #1

---
+++ src/scrape/db.py
@@ -0,0 +1,51 @@
+from typing import List
+from typing import Optional
+from sqlalchemy import (
+    String,
+    Column,
+    Integer,
+    String,
+    DateTime,
+    func,
+    Text,
+    create_engine,
+    select,
+)
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import Session
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Raw_Data(Base):
+    __tablename__ = "raw_data"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    source: Mapped[str] = mapped_column(Text)
+    content: Mapped[str] = mapped_column(Text)
+    remarks: Mapped[str] = mapped_column(Text)
+    created_at = mapped_column(DateTime, default=func.now())
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "postgresql://postgres:Iamalextay96@192.168.1.3:5433/nlp", echo=True
+    )
+    Base.metadata.create_all(engine)
+    with Session(engine) as session:
+        # spongebob = Raw_Data(
+        #     source="malaysiakini",
+        #     content="Spongebob Squarepants",
+        #     remarks="spongebob@sqlalchemy.org",
+        # )
+
+        # session.add_all([spongebob])
+        # session.commit()
+        stmt = select(Raw_Data).where(Raw_Data.source.in_(["malaysiakini", "sandy"]))
+        queryResults = session.scalars(stmt).all()
+        print(queryResults)
